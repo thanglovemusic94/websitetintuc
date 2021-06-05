8f95f0bc9ed94ecba38f6e6d29b1f97c2d174f00
thangnv <thangnv@mintpotvn.com>
2021-06-06 03:22:56 +0700

    init project

---
+++ src/main/java/com/thangtony/websitetintuc/WebsitetintucApplication.java
@@ -0,0 +1,13 @@
+package com.thangtony.websitetintuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsitetintucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsitetintucApplication.class, args);
+    }
+
+}
